ea1df678e54a8751ddcdc7b527d28332a2e4bcce
milsab <milad.sabouri@gmail.com>
2017-09-08 18:15:11 -0500

    Basic Interactivity

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,5 +1,8 @@
 var main = function(){
 	"use strict";
+	$(".comment-input button").on("click", function(event){
+		$('.comments').append('<p> new Comment</p>');
+	});
 };
 
 $(document).ready(main);
