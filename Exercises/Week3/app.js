8bc8e985e71fe712b014dc68d698240b4a60c251
milsab <milad.sabouri@gmail.com>
2017-09-09 11:11:28 -0500

    final Comment Application

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,8 +1,26 @@
 var main = function(){
 	"use strict";
 	$(".comment-input button").on("click", function(event){
-		$('.comments').append('<p> new Comment</p>');
+		addCommnet();
 	});
+
+	$('.comment-input input').on('keypress',function(event){		
+		if(event.keyCode === 13){
+			addCommnet();		
+		}
+	});
+
+	function addCommnet(){
+		var $new_comment;
+		if($(".comment-input input").val() !== ''){
+			var $commnet_text = $(".comment-input input").val();
+			$new_comment = $('<p>').text($commnet_text);
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$('.comment-input input').val('');
+		}
+	}
 };
 
 $(document).ready(main);
